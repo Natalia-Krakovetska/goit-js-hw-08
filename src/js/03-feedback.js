a70efaeda91dea5956d93dac29b4bb650aad346c
Natalia <natalakrakoveckaya@gmail.com>
2022-11-06 22:15:10 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,48 +1,48 @@
 import throttle from 'lodash.throttle';
-console.log(throttle);
+
 const refs ={
     form: document.querySelector('.feedback-form'),
-    email: document.querySelector('input'),
-    message: document.querySelector('textarea'),
     btn: document.querySelector('button'),
+    input: document.querySelector('input'),
+    textarea: document.querySelector('textarea')
 
 };
-console.log(refs);
 
-const currentData = {};
+
+
+
+
+
+
+refs.form.addEventListener('input', inFormInput)
+refs.form.addEventListener('submit', onSubmitBtn);
+
+let currentData = {};
+let savedData = {};
 const STORAGE_KEY = "feedback-form-state";
-// refs.email.addEventListener('input', onInput);
-// refs.message.addEventListener('input', onInputTextarea);
-// refs.form.addEventListener('submit',onInput)
-refs.form.addEventListener('input', inFormSubmit)
-// populateData ();
+populateData ();
 
-function inFormSubmit(event){
+function inFormInput(event){
+   
     event.preventDefault();
-    currentData[event.currentTarget.name] = event.currentTarget.value;
-    console.log(currentData);
-   const hhh = localStorage.setItem(STORAGE_KEY, JSON.stringify(currentData)) || [];
-    // event.currentTarget.reset();
-    // localStorage.removeItem(STORAGE_KEY);
+    currentData[event.target.name] = event.target.value;
+    
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(currentData));  
+    
 };
 
-// function  onInput(event){
-//     console.log(event.target.value);
-//     localStorage.setItem(STORAGE_KEY,event.target.value)
-// };
-// function  onInputTextarea(event){
-  
-//     console.log(event.target.value);
-//     localStorage.setItem(STORAGE_KEY,event.target.value)
-// };
+function populateData(){
+    savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+ if(savedData){
+    refs.input.value = savedData.email;
+    refs.textarea.value = savedData.message;
+    console.log(savedData);       
+}
+}
 
-// function populateData(){
-// const feedBack = JSON.parse(STORAGE_KEY);
-// const savedData = localStorage.getItem(feedBack);
-// if(savedData){
-//     console.log(savedData);
-//     refs.email = savedData;
-// }
-// }
-
-
+function onSubmitBtn(event){    
+    console.log(savedData);
+    event.currentTarget.reset();    
+    localStorage.removeItem(STORAGE_KEY);
+    
+};
