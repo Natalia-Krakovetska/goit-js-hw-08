df5456fd285112ed78246938ed105d6177d17f84
Natalia <natalakrakoveckaya@gmail.com>
2022-11-07 05:05:26 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,43 +1,43 @@
 import throttle from 'lodash.throttle';
 
-const refs ={
-    form: document.querySelector('.feedback-form'),
-    btn: document.querySelector('button'),
-    input: document.querySelector('input'),
-    textarea: document.querySelector('textarea')
-
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  btn: document.querySelector('button'),
+  input: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
 };
 
-refs.form.addEventListener('input', inFormInput)
-refs.form.addEventListener('submit', onSubmitBtn);
+refs.form.addEventListener('input', throttle(inFormInput, 500));
+refs.form.addEventListener('submit', onSubmitForm);
 
 let currentData = {};
 let savedData = {};
-const STORAGE_KEY = "feedback-form-state";
-populateData ();
+const STORAGE_KEY = 'feedback-form-state';
+populateData();
 
+function inFormInput(event) {
+  event.preventDefault();
+  currentData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(currentData));
+}
 
-function inFormInput(event){
-   
-    event.preventDefault();
-    currentData[event.target.name] = event.target.value;
-    
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(currentData));  
-    
-};
-
-function populateData(){
+function populateData() {
+  try {
     savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
- if(savedData){
+  } catch (error) {
+    console.log(error.name); // "SyntaxError"
+    console.log(error.message); // Unexpected token W in JSON at position 0
+  }
+
+  if (savedData) {
     refs.input.value = savedData.email;
     refs.textarea.value = savedData.message;
-    console.log(savedData);       
-}
+  }
 }
 
-function onSubmitBtn(event){    
-    
-    event.currentTarget.reset();    
-    localStorage.removeItem(STORAGE_KEY);
-    
-};
+function onSubmitForm(event) {
+  event.preventDefault();
+  console.log(currentData);
+  event.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
